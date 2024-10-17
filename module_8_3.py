6e80068beb45c6e859db758f2f34921dbfce1806
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-18 01:59:11 +0300

    done

--- module_8_3.py
+++ module_8_3.py
@@ -1,5 +1,93 @@
+class IncorrectVinNumber(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message: str):
+        self.message = message
+
+
+class Car:
+    def __init__(self, model: str, vin: int, numbers: str):
+        """
+        :param model: название автомобиля (строка)
+        :param vin: vin номер автомобиля (целое число). Уровень доступа private.
+        :param numbers: номера автомобиля (строка).
+        """
+        if self.__is_valid_vin(vin) and self.__is_valid_numbers(numbers):
+            self.__vin = vin
+            self.model = model
+            self.__numbers = numbers
+
+    def __is_valid_vin(self, vin_number: int) -> bool:
+        """
+        Уровень доступа private. Проверяет vin_number на корректность.
+        :param vin_number: vin номер автомобиля (целое число)
+        :return: True, если корректный, в других случаях выбрасывает исключение.
+        """
+        # Выбрасывает исключение IncorrectVinNumber с сообщением 'Некорректный тип vin номер', если передано не целое число
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номера')
+
+        # Выбрасывает исключение IncorrectVinNumber с сообщением 'Неверный диапазон для vin номера',
+        # если переданное число находится не в диапазоне от 1000000 до 9999999 включительно.
+        if not (1000000 <= vin_number <= 9999999):
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+
+        return True
+
+    def __is_valid_numbers(self, numbers: str) -> bool:
+        """
+        Уровень доступа private. Проверяет numbers на корректность.
+        :param numbers: номера автомобиля (строка).
+        :return: True, если корректный, в других случаях выбрасывает исключение.
+        """
+        # Выбрасывает исключение IncorrectCarNumbers с сообщением 'Некорректный тип данных для номеров', если передана не строка.
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+
+        # Выбрасывает исключение IncorrectCarNumbers с сообщением 'Неверная длина номера', переданная строка должна состоять ровно из 6 символов.
+        if len(numbers) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+
+        return True
+
+
 def test():
-    pass
+    try:
+        first = Car('Model1', 1000000, 'f123dj')
+    except IncorrectVinNumber as exc:
+        print(exc.message)
+    except IncorrectCarNumbers as exc:
+        print(exc.message)
+    else:
+        print(f'{first.model} успешно создан')
+
+    try:
+        second = Car('Model2', 300, 'т001тр')
+    except IncorrectVinNumber as exc:
+        print(exc.message)
+    except IncorrectCarNumbers as exc:
+        print(exc.message)
+    else:
+        print(f'{second.model} успешно создан')
+
+    try:
+        third = Car('Model3', 2020202, 'нет номера')
+    except IncorrectVinNumber as exc:
+        print(exc.message)
+    except IncorrectCarNumbers as exc:
+        print(exc.message)
+    else:
+        print(f'{third.model} успешно создан')
+
+    """
+    Вывод на консоль:
+    Model1 успешно создан
+    Неверный диапазон для vin номера
+    Неверная длина номера
+    """
 
 
 if __name__ == '__main__':
